5d4018b8db426222a97dc808efd39e93ee1a5e1a
JIMANTHA <jwengler@gmail.com>
2019-07-12 16:30:26 -0600

    Changed file paths to support other systems

--- Bash/runNew.sh
+++ Bash/runNew.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-clang++ -std=gnu++14 ../mmap_test_dev/mmap_test_dev/main.cpp -o NewMmap
+g++ -std=gnu++14 ../mmap_test_dev/mmap_test_dev/main.cpp -o NewMmap
 
-time ./NewMmap /Users/jameswengler/TFB/TFB_DATA/100_900_1000000.fwf2.ll /Users/jameswengler/TFB/TFB_DATA/100_900_1000000.fwf2 /Users/jameswengler/TFB/TFB_DATA/100_900_1000000.fwf2.cc /Users/jameswengler/TFB/Output/NewOutputC++.txt /Users/jameswengler/TFB/TFB_DATA/100_900_1000000.fwf2.mccl /Users/jameswengler/TFB/TFB_DATA/100_900_1000000_columns.tsv 1000000
+time ./NewMmap ../TFB_DATA/100_900_1000000.fwf2.ll ../TFB_DATA/100_900_1000000.fwf2 ../TFB_DATA/100_900_1000000.fwf2.cc ../Output/NewOutputC++.txt ../TFB_DATA/100_900_1000000.fwf2.mccl ../TFB_DATA/100_900_1000000_columns.tsv 1000000
 echo "New Code^"
--- Bash/runOld.sh
+++ Bash/runOld.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 g++ -std=gnu++14 ../mmap_test/mmap_test/main.cpp -o mmap
 
-time ./mmap /Users/jameswengler/TFB/TFB_DATA/100_900_1000000.fwf2.ll /Users/jameswengler/TFB/TFB_DATA/100_900_1000000.fwf2 /Users/jameswengler/TFB/TFB_DATA/100_900_1000000.fwf2.cc /Users/jameswengler/TFB/Output/OldOutputC++.txt /Users/jameswengler/TFB/TFB_DATA/100_900_1000000.fwf2.mccl /Users/jameswengler/TFB/TFB_DATA/100_900_1000000_columns.tsv 1000000
+time ./mmap ../TFB_DATA/100_900_1000000.fwf2.ll ../TFB_DATA/100_900_1000000.fwf2 ../TFB_DATA/100_900_1000000.fwf2.cc ../Output/OldOutputC++.txt ../TFB_DATA/100_900_1000000.fwf2.mccl ../TFB_DATA/100_900_1000000_columns.tsv 1000000
 echo "Old Code ^"

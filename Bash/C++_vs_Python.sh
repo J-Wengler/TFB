8b9a6a108a831865ba1de2b5c255dd6592c6dc26
JIMANTHA <jwengler@gmail.com>
2019-07-16 11:33:40 -0600

    Changed python to python3

--- Bash/C++_vs_Python.sh
+++ Bash/C++_vs_Python.sh
@@ -6,4 +6,4 @@ echo  -e "\033[33;5mWaiting\033[0m"
 echo
 wait
 echo "Testing Files for differences"
-python ../Python/CompareFiles.py ../Output/OutputPY.txt ../Output/NewOutputC++.txt no_line
+python3 ../Python/CompareFiles.py ../Output/OutputPY.txt ../Output/NewOutputC++.txt no_line
--- Bash/runPython.sh
+++ Bash/runPython.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-time python ~/TFB/Python/TestFixedWidth2.py ../TFB_DATA/100_900_1000000.fwf2 ../TFB_DATA/100_900_1000000_columns.tsv ~/TFB/Output/OutputPY.txt 1000000
+time python3 ~/TFB/Python/TestFixedWidth2.py ../TFB_DATA/100_900_1000000.fwf2 ../TFB_DATA/100_900_1000000_columns.tsv ~/TFB/Output/OutputPY.txt 1000000
 echo "Python Code^"
 

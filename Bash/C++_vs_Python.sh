ce9498c3f17dda15cd77daa08a2694d630899cf9
JIMANTHA <jwengler@gmail.com>
2019-07-16 11:08:02 -0600

    Created \'C++_vs_Python\'

---
+++ Bash/C++_vs_Python.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Running both versions of code as parallel processes"
+echo  -e "\033[33;5mWaiting\033[0m"
+(bash ./runNew.sh) &
+(bash ./runPython.sh)
+echo
+wait
+echo "Testing Files for differences"
+python ../Python/CompareFiles.py ../Output/OldOutputC++.txt ../Output/NewOutputC++.txt no_line

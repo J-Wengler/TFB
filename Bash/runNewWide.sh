477db3e41f20ad4b849d1ef9fc39742b880f2769
JIMANTHA <jwengler@gmail.com>
2019-07-18 11:35:35 -0600

    Added tests for wide files

---
+++ Bash/runNewWide.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+g++ -std=gnu++14 ../mmap_test_dev/mmap_test_dev/main.cpp -o NewMmap 
+time ./NewMmap ../TFB_DATA/100000_900000_1000.fwf2.ll ../TFB_DATA/100000_900000_1000.fwf2 ../TFB_DATA/100000_900000_1000.fwf2.cc ../Output/NewOutputC++.txt ../TFB_DATA/100000_900000_1000.fwf2.mccl ../TFB_DATA/100000_900000_1000_columns.tsv 1000
+echo "New Code^"

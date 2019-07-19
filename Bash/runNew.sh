1aa9a19bc9440aca562cdbbc6f6a52f7fb26e7d3
JIMANTHA <jwengler@gmail.com>
2019-07-19 15:44:34 -0600

    Optimized g++ compiler

--- Bash/runNew.sh
+++ Bash/runNew.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-g++ -std=gnu++14 ../mmap_test_dev/mmap_test_dev/main.cpp -o NewMmap
+g++  -O3 -std=gnu++14 ../mmap_test_dev/mmap_test_dev/main.cpp -o NewMmap
 
 time ./NewMmap ../TFB_DATA/100_900_1000000.fwf2.ll ../TFB_DATA/100_900_1000000.fwf2 ../TFB_DATA/100_900_1000000.fwf2.cc ../Output/NewOutputC++.txt ../TFB_DATA/100_900_1000000.fwf2.mccl ../TFB_DATA/100_900_1000000_columns.tsv 1000000
 echo "New Code^"

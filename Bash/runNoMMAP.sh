a388734046ed14546e4747d47ae9d2ea16d937c1
JIMANTHA <jwengler@gmail.com>
2019-08-01 11:51:37 -0600

    Changed output file on runNoMMAP.sh

--- Bash/runNoMMAP.sh
+++ Bash/runNoMMAP.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 g++  -O3 -std=gnu++14 ../TestFixedWidth2_NOMMAP/TestFixedWidth2_NOMMAP/main.cpp -o NO_Mmap
 
-time ./NO_Mmap ../TFB_DATA/100_900_1000000.fwf2.ll ../TFB_DATA/100_900_1000000.fwf2 ../TFB_DATA/100_900_1000000.fwf2.cc ../Output/NewOutputC++.txt ../TFB_DATA/100_900_1000000.fwf2.mccl ../TFB_DATA/100_900_1000000_columns.tsv 1000000 
+time ./NO_Mmap ../TFB_DATA/100_900_1000000.fwf2.ll ../TFB_DATA/100_900_1000000.fwf2 ../TFB_DATA/100_900_1000000.fwf2.cc ../Output/NoMMapOutputC++.txt ../TFB_DATA/100_900_1000000.fwf2.mccl ../TFB_DATA/100_900_1000000_columns.tsv 1000000 
 echo "Using Lseek^"

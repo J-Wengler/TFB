3808c6f3fd60e2b77043f7e9615b8d22ce4c8489
JIMANTHA <jwengler@gmail.com>
2019-08-05 12:42:52 -0600

    Fixed C++ vs Python tests to use Old Code

--- Bash/C++_vs_PythonTall.sh
+++ Bash/C++_vs_PythonTall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Running both versions of code as parallel processes"
-(bash ./runNew.sh) &
+(bash ./runOld.sh) &
 (bash ./runPython.sh)
 echo
 wait
 echo "Testing Files for differences"
-python3 ../Python/CompareFiles.py ../Output/OutputPY.txt ../Output/NewOutputC++.txt no_line
+python3 ../Python/CompareFiles.py ../Output/OutputPY.txt ../Output/OldOutputC++.txt no_line
--- Bash/C++_vs_PythonWide.sh
+++ Bash/C++_vs_PythonWide.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Running both versions of code as parallel processes"
-(bash ./runNewWide.sh) &
+(bash ./runOldWide.sh) &
 (bash ./runPythonWide.sh)
 echo
 wait
 echo "Testing Files for differences"
-python3 ../Python/CompareFiles.py ../Output/OutputPY.txt ../Output/NewOutputC++.txt no_line
+python3 ../Python/CompareFiles.py ../Output/OutputPY.txt ../Output/OldOutputC++.txt no_line

3d9af8c644b009087596ccd6ef71537f9b4fa679
JIMANTHA <jwengler@gmail.com>
2019-09-04 09:27:51 -0600

    Fixed timing in runParallelTestTFW3.sh

--- Bash/runParallelTestTFW3.sh
+++ Bash/runParallelTestTFW3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Running both versions of code as parallel processes"
 (bash ./runTFW3C++.sh) & (bash ./runTFW3PY.sh)
+echo 
+wait
 echo "Testing Files for Differences"
 python3 ../Python/CompareFiles.py ../Output/OutputPY.txt ../Output/OutputTFW3++.txt no_line
